5c94ebfd8ec888586eda9d29228cbf562f20f29c
Shaun <shaun@thenetninja.co.uk>
2022-03-29 11:57:57 +0100

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [solution, setSolution] = useState(null)
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        // random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
-  );
+  )
 }
 
 export default App
