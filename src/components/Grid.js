bde10d59029ef12499eda3d1391ae1a2e7ccb8c1
Shaun <shaun@thenetninja.co.uk>
2022-03-30 11:37:05 +0100

    lesson-10

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,6 +7,9 @@ export default function Grid({ guesses, currentGuess, turn }) {
   return (
     <div>
       {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />
+        }
         return <Row key={i} guess={g} /> 
       })}
     </div>
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 
   if (guess) {
     return (
       <div className="row past">
         {guess.map((l, i) => (
           <div key={i} className={l.color}>{l.key}</div>
+        ))}
+      </div>
+    )
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split('')
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">{letter}</div>
+        ))}
+        {[...Array(5 - letters.length)].map((_,i) => (
+          <div key={i}></div>
         ))}
       </div>
     )
