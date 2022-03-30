326310773caa07fb1557e0a640d77d83567550fd
Shaun <shaun@thenetninja.co.uk>
2022-03-30 12:22:08 +0100

    lesson-15

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -11,12 +11,17 @@ export default function Wordle({ solution }) {
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup)
 
-    return () => window.removeEventListener('keyup', handleKeyup)
-  }, [handleKeyup])
+    if (isCorrect) {
+      console.log('congrats, you win')
+      window.removeEventListener('keyup', handleKeyup)
+    }
+    if (turn > 5) {
+      console.log('unlucky, out of guesses')
+      window.removeEventListener('keyup', handleKeyup)
+    }
 
-  useEffect(() => {
-    console.log(guesses, turn, isCorrect)
-  }, [guesses, turn, isCorrect])
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleKeyup, isCorrect, turn])
 
   return (
     <div>
