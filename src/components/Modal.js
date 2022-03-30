0cede0db73f0aaf2edc924e981b81336d30b7355
Shaun <shaun@thenetninja.co.uk>
2022-03-30 13:18:13 +0100

    lesson-16

---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Modal({ isCorrect, solution, turn }) {
+  return (
+    <div className="modal">
+      {isCorrect && (
+        <div>
+          <h1>You Win!</h1>
+          <p className="solution">{solution}</p>
+          <p>You found the solution in {turn} guesses :)</p>
+        </div>
+      )}
+      {!isCorrect && (
+        <div>
+          <h1>Nevermind</h1>
+          <p className="solution">{solution}</p>
+          <p>Better luck next time :)</p>
+        </div>
+      )}
+    </div>
+  )
+}
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import useWordle from '../hooks/useWordle'
 
 // components
 import Grid from './Grid'
 import Keypad from './Keypad'
+import Modal from './Modal'
 
 export default function Wordle({ solution }) {
   const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = useWordle(solution)
+  const [showModal, setShowModal] = useState(false)
   
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup)
 
     if (isCorrect) {
-      console.log('congrats, you win')
+      setTimeout(() => setShowModal(true), 2000)
       window.removeEventListener('keyup', handleKeyup)
     }
     if (turn > 5) {
-      console.log('unlucky, out of guesses')
+      setTimeout(() => setShowModal(true), 2000)
       window.removeEventListener('keyup', handleKeyup)
     }
 
@@ -25,10 +27,9 @@ export default function Wordle({ solution }) {
 
   return (
     <div>
-      <div>solution - {solution}</div>
-      <div>Current Guess - {currentGuess}</div>
       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution} />}
     </div>
   )
 }
